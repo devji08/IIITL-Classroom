b620ce9d3805a36ed3d7bd0288300e13c134727b
devji08 <devsharma23101999@gmail.com>
2021-05-21 14:27:59 +0530

    quiz UI

--- components/AssignmentComponent.js
+++ components/AssignmentComponent.js
@@ -10,49 +10,12 @@ class AssignmentComponent extends Component {
         var d = new Date(this.props.ass.postdate.seconds * 1000);
         var postdate = (d.toDateString().substring(4,10));
         var duedate = (new Date(this.props.ass.due.seconds * 1000)).toDateString().substring(4,10);
-        if(this.props.ass.id == "ass"){
-            return(
-                <TouchableOpacity 
-                                activeOpacity = {0.5}
-                                onPress = {() => { Linking.openURL(this.props.ass.file)}}
-                                style = {styles.container}
-                            >
-                    <View >
-                        <View style = {styles.top}>
-                            <Text style = {{fontWeight:'bold', fontSize : 18, paddingBottom : 5, color : '#5b0a91'}}>{this.props.ass.title}</Text>
-                            <Text style = {{color:'gray', paddingBottom : 5}}>{this.props.ass.professor} - {postdate}</Text>
-                            <View style={{flexDirection : 'row'}}>
-                                <Text style = {{paddingBottom : 10}}>{this.props.ass.points} points</Text>
-                                <Text style = {{textAlign : 'right', flex : 1}}>Due {duedate}</Text>
-                            </View>
-                        </View>
-                        <View style = {styles.bottom}>
-                            <Text>{this.props.ass.description}</Text>
-                            
-                            
-                                <View style={{flexDirection : 'row'}}>
-                                    {<View style= {{flex : 1, paddingTop : 10}}>
-                                        <Icon
-                                            type = 'font-awesome-5'
-                                            name = 'file-pdf'
-                                            size = {50}
-                                            color = '#a62c2b'
-                                            containerStyle = {{alignItems : 'flex-start'}}
-                                        />
-                                    </View>}
-                                    <Text style={{flex:6, alignSelf : 'center', color : 'gray'}}>Assignment.pdf</Text>
-                                </View>
-                        </View>
-                    </View>
-                </TouchableOpacity>
-            )
-        }
-        else return (
+        return(
             <TouchableOpacity 
-                                activeOpacity = {0.5}
-                                onPress = {() => { this.props.navigate('Quiz',{quiz : this.props.ass})}}
-                                style = {styles.container}
-                            >
+                            activeOpacity = {0.5}
+                            onPress = {() => { this.props.navigate('Quiz',{data : this.props.ass})}}
+                            style = {styles.container}
+                        >
                 <View >
                     <View style = {styles.top}>
                         <Text style = {{fontWeight:'bold', fontSize : 18, paddingBottom : 5, color : '#5b0a91'}}>{this.props.ass.title}</Text>
@@ -64,10 +27,25 @@ class AssignmentComponent extends Component {
                     </View>
                     <View style = {styles.bottom}>
                         <Text>{this.props.ass.description}</Text>
+                        
+                        
+                            <View style={{flexDirection : 'row'}}>
+                                {<View style= {{flex : 1, paddingTop : 10}}>
+                                    <Icon
+                                        type = 'font-awesome-5'
+                                        name = 'file-pdf'
+                                        size = {50}
+                                        color = '#a62c2b'
+                                        containerStyle = {{alignItems : 'flex-start'}}
+                                    />
+                                </View>}
+                                <Text style={{flex:6, alignSelf : 'center', color : 'gray'}}>{this.props.ass.title}.pdf</Text>
+                            </View>
                     </View>
                 </View>
             </TouchableOpacity>
         )
+      
     }
 }
 
--- components/QuizComponent.js
+++ components/QuizComponent.js
@@ -5,17 +5,93 @@ import {Icon} from 'react-native-elements'
 class QuizComponent extends Component {
 
     state = {
-        quiz : this.props.route.params.quiz,
+        data : this.props.route.params.data,
     };
 
     render() {
-        console.log(this.state.quiz);
+        const navigation = this.props.navigation;
+        navigation.setOptions({title : this.state.data.subname});
+
+        var d = new Date(this.state.data.postdate.seconds * 1000);
+        var postdate = (d.toDateString().substring(0,10));
+        d = new Date(this.state.data.due.seconds * 1000);
+        var duedate = (d.toDateString().substring(0,10));
+        console.log(this.state.data, postdate, duedate);
+
         return (
-            <View>
+            <View style = {styles.container}>
+                <View>
+                    <View style = {{
+                        borderBottomWidth : 1,
+                        borderBottomColor : '#4285f4',
+                        marginBottom : 10,
+                    }}>
+                        <View>                        
+                            <Text
+                                style = {{
+                                    fontSize : 30,
+                                    color : '#1967d2',
+                                    fontFamily : 'Roboto',
+                                    fontWeight : '400'
+                                }}
+                            >{this.state.data.title}</Text>
+                        </View>
+                        
+                        <View>
+                            <Text
+                                style = {{
+                                    fontSize : 15,
+                                    color : '#5f6368',
+                                    fontFamily : 'sans-serif',
+                                    fontWeight : '400'
+                                }}
+                            >{this.state.data.professor} • {postdate}</Text>
+                        </View>
+                        <View>
+                            <Text
+                                style = {{
+                                    fontSize : 15,
+                                    color : '#3c4043',
+                                    fontFamily : 'Roboto',
+                                    fontWeight : '500'
+                                }}
+                            >{duedate}</Text>
+                        </View>
+                        <View>
+                            <Text
+                                style = {{
+                                    fontSize : 15,
+                                    color : '#3c4043',
+                                    fontFamily : 'Roboto',
+                                    fontWeight : '400'
+                                }}
+                            >{this.state.data.description}</Text>
+                        </View>
+                    </View>
+                </View>
 
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container : {
+        backgroundColor : 'white',
+        padding : 10,
+    },
+    header : {
+        borderBottomWidth : 1,
+        borderBottomColor : '#4285f4',
+        marginBottom : 10,
+    },
+    box : {
+        backgroundColor : 'white',
+        borderRadius : 15,
+        elevation : 3,
+        padding : 5,
+    },
+    
+})
 
 export default QuizComponent;
